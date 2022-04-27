f62e457f0638c46a0ca1193335e5c36bf01c4d9c
davin <christophorusdavin@gmail.com>
2022-04-27 13:46:13 +0700

    Initial Commit

---
+++ Nano1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nano1
+//
+//  Created by Christophorus Davin on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
